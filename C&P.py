c19f557d48a25c8559f57ad7b88d1574dea4ca5e
kkuoii007-coder <kkuoii007@gmail.com>
2026-02-03 14:26:35 +0300

    correct_code

--- C&P.py
+++ C&P.py
@@ -1,8 +1,3 @@
-
-
-#3. Продемонстрируйте полиморфизм: создайте функцию `animal_sound(animals)`, которая принимает список животных и вызывает метод `make_sound()` для каждого животного.
-#4. Используйте композицию для создания класса `Zoo`, который будет содержать информацию о животных и сотрудниках. Должны быть методы для добавления животных и сотрудников в зоопарк.
-#5. Создайте классы для сотрудников, например, `ZooKeeper`, `Veterinarian`, которые могут иметь специфические методы (например, `feed_animal()` для `ZooKeeper` и `heal_animal()` для `Veterinarian`).
 class Animal():
     def __init__(self, name, age):
         self.name = name
@@ -48,7 +43,7 @@ class Person:
         self.nationality = nationality
 
     def care(self):
-        print(f"{self.name} заботиться о животных")
+        print(f"{self.name} заботится о животных")
 
 class ZooKeeper(Person):
     def __init__(self, name, nationality):
@@ -57,12 +52,59 @@ class ZooKeeper(Person):
     def feed_animal(self):
         print(f"{self.name} покормил животное")
 
+class Veterinarian(Person):
+    def __init__(self, name, nationality):
+        super().__init__(name, nationality)
+
+    def heal_animal(self):
+        print(f"{self.name} излечил животное")
+
+class Zoo:
+    def __init__(self):
+        self.animals = []
+        self.persons = []
+
+    def add_animal(self, animal):
+        self.animals.append(animal)
+
+    def add_person(self, person):
+        self.persons.append(person)
+
+    def show_animals(self):
+        print("\nЖивотные в зоопарке:")
+        for animal in self.animals:
+            print(f"- {animal.__class__.__name__}: name={animal.name}, age={animal.age}")
+
+    def show_persons(self):
+        print("\nСотрудники зоопарка:")
+        for per in self.persons:
+            print(f"- {per.__class__.__name__}: name={per.name}, nationality={per.nationality}")
 
 
-a = Bird("Грего", 5, "красный")
-b = Mammal("Петр", 56, 70)
-c = Reptile("Севик", 6, "Европа")
 
+zoo = Zoo()
 
-animals = [a, b, c]
-make_animal_sound(animals)
+bird = Bird("Кеша", 2, "green")
+mammal = Mammal("Барсик", 5, 4.2)
+reptile = Reptile("Гоша", 3, "desert")
+
+zoo.add_animal(bird)
+zoo.add_animal(mammal)
+zoo.add_animal(reptile)
+
+keeper = ZooKeeper("Паша", "RU")
+vet = Veterinarian("Ольга", "RU")
+
+zoo.add_person(keeper)
+zoo.add_person(vet)
+
+make_animal_sound(zoo.animals)
+
+zoo.show_animals()
+zoo.show_persons()
+
+print("\nДействия сотрудников:")
+keeper.care()
+keeper.feed_animal()
+vet.care()
+vet.heal_animal()
